a7fd2948f3e806e19223099fd00518c4f7d35d3b
Roman Zhydyk <roman@student.dei.uc.pt>
2020-09-22 18:15:29 +0100

    added header component and links to home, shop and contact

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component.jsx'
+import Header from './components/header/header.component'
 // Prop tunneling or drilling is a bad pattern
 // So use HOC withRouter()
 
 function App() {
   return (
     <div>
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route exact path='/shop' component={ShopPage}/>
-    </Switch>       
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>       
     </div>
   );
 }
