8cb77d249ad1e6bb2d4416aeb2c1bd5c3aedc309
Roman Zhydyk <roman@student.dei.uc.pt>
2020-10-26 12:04:46 +0000

    Testing styled componenents on some existing components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import './header.styles.scss'
-import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firebase.utils'
-
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import { connect } from 'react-redux'
@@ -13,32 +10,33 @@ import { createStructuredSelector } from "reselect";
 import { selectCartHidden } from '../../redux/cart/cart.selectors.js'
 import { selectCurrentUser } from '../../redux/user/user.selectors.js'
 
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.styles'
 
 const Header = ({ currentUser, hidden }) => {
     return (
-        <div className='header'>
-            <Link className='logo-container' to='/'>
+        <HeaderContainer>
+            <LogoContainer to='/'>
                 <Logo className='logo'/>
-            </Link>
-            <div className='options'>
-                <Link className='option' to='/shop'>SHOP</Link>
-                <Link className='option' to='/shop'>CONTACT</Link>
+            </LogoContainer>
+            <OptionsContainer>
+                <OptionLink to='/shop'>SHOP</OptionLink>
+                <OptionLink to='/shop'>CONTACT</OptionLink>
                 {
                 currentUser ?
-                (<div className='option' onClick={ () => auth.signOut() }>
+                (<OptionLink as='div' onClick={ () => auth.signOut() }>
                     SIGN OUT
-                </div>)
+                </OptionLink>)
                 :
-                (<Link className='option' to='/signin'>
+                (<OptionLink to='/signin'>
                     SIGN IN
-                </Link>)
+                </OptionLink>)
                 }
                 <CartIcon />
-            </div>
+            </OptionsContainer>
             {
                 hidden ? null : <CartDropdown />
             }
-        </div>
+        </HeaderContainer>
     )
 }
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
-import './homepage.styles.scss'
 import Directory from '../../components/directory/directory.component'
 
-// Its a functional component because we don't really need any lifecycle methods at this point,
-// not do we to store any state
+import { HomePageContainer } from './homepage.styles'
+
 const HomePage = () => {
     return (
-      <div className="homepage">
+      <HomePageContainer>
         <Directory />
-      </div>
+      </HomePageContainer>
     )
 }
 
